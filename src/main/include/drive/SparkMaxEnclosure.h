c6f4313a2b37a8b5dffe0989b0d83ae918bcfbc0
Rob Heslin <Rob.Heslin@gmail.com>
2019-11-27 18:12:00 -0500

    Update SparkMaxEnclosure.h with documentation
    Documentation is in the form of comments that enhance the usability of the library.

--- src/main/include/drive/SparkMaxEnclosure.h
+++ src/main/include/drive/SparkMaxEnclosure.h
@@ -8,7 +8,10 @@
 
 #include <math.h>
 
-// const double kGearRatio = 18.0/68;
+// The 2019 swerve prototype has a gear 18 teeth on the driving motor output.
+// The 2019 swerve prototype has 68 teeth on the driven/rotating module.
+// The 2019 swerve prototype uses a Vex mag encoder whihc has 1024 ticks per rotation.
+// The constant kGearRatio is in units of encoder ticks per module revolution. I.E. 1024*68/18 for 2019 prototype
 const double kGearRatio = 39; //39
 
 /*
@@ -42,24 +45,34 @@ public:
 					int turnMotor);
 	~SparkMaxEnclosure();
 
-	/*
+	/**
 	 * Move the wheel to the given speed and rotational values.
 	 * Method is used by RobotDriveSwerve for controlling each individual swerve
 	 * wheel.
+	 * @param speedVal The speed of the moveMotor with input range -1 to 1 for PercentVBus mode 
+	 * @param rotationVal The rotational position of the swerve module in units of revolutions of the module
 	 */
 	void MoveWheel(double speedVal, double rotationVal) override;
-	/*
+	
+	/**
 	 * Stops all movement of an enclosures motors
 	 */
 	void StopWheel() override;
-	/*
+	
+	/**
 	 * Used to invert a corresponding speed controller, based off of the motor type
 	 * given.  If the value is set to true, the controller is inverted, and if the
-	 * value is set to false. the controller is uninverted.
+	 * value is set to false. the controller is uninverted. This function does not 
+	 * switch the direction of the motor with respect to the encoder for the module
+	 * turnMotor.
 	 */
 	void SetInverted(MotorType type, bool val);
-	/*
-	 * Sets the PIDF value being used by a swerve enclosure
+	
+	/**
+	 * Sets the PIDF value being used by a swerve enclosure. If all values are
+	 * zero the swerve module will not spin. Thesevalues must be tuned to optimize
+	 * the modules turn while under proper weight and on the appropriate driving
+	 * surface.
 	 */
 	void SetPID(double P, double I, double D, double F = 0);
 	/*
@@ -70,8 +83,11 @@ public:
 	 * Returns the name of the enclosure
 	 */
 	std::string GetName() override;
-	/*
-	 * Reverse encoder direction
+	
+	/**
+	 * Reverse encoder direction for the turnMotor. This should not be used 
+	 * to switch the direction of the turnMotor encoder with respect to the
+	 * turnMotor output. Instead us
 	 */
 	void SetReverseEncoder(bool reverseEncoder);
 	/*
@@ -89,12 +105,20 @@ private:
 	 * Sets the speed value to the movement motor
 	 */
 	void SetSpeed(double speedVal);
-	/*
-	 * Sets the that the wheel should turn to
+	
+	/**
+	 * Sets the that the wheel should turn to using the TalonSRX's internal PID loop.
+	 * @param 
 	 */
 	void SetAngle(double rotationVal);
-	/*
-	 * Converts the given angle to a range of -0.5 to 0.5.
+	
+	/**
+	 * Converts the given angle to a range of -0.5 to 0.5. Remember that one
+	 * rotation of the module is an angle of 1, so the range -0.5 to 0.5 is 
+	 * one full rotation of the module
+	 * @param angle The angle that the module should turn to. In units of revolutions
+	 * @param encoderValue The tik count of the encoders. In units of ticks
+	 * @return angle that the module should turn to, in a range of -.0.5 to 0.5, in units of revolutions
 	 */
 	double ConvertAngle(double angle, double encoderValue);
 
@@ -104,7 +128,7 @@ private:
 	WPI_TalonSRX turnMotor;
 
 	std::string name;
-	double gearRatio = 9;
+	double gearRatio = 9;//Is this used anywhere?
 	bool reverseEncoder = false;
 	bool reverseSteer = false;
 };
